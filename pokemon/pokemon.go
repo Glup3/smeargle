678f204c47b6f430ce41cdaaa718ba66885c113c
Phuc Tran <glup3.tran@gmail.com>
2025-02-20 18:16:42 +0100

    fix black blocks

--- pokemon/pokemon.go
+++ pokemon/pokemon.go
@@ -6,7 +6,11 @@ import (
 	"strings"
 )
 
-const blockRune = '▀'
+const (
+	upperBlock = '▀'
+	lowerBlock = '▄'
+	emptyBlock = ' '
+)
 
 type Pokemon struct {
 	Name  string
@@ -25,14 +29,31 @@ func (p Pokemon) String() string {
 
 	minX, minY, maxX, maxY := findVisibleBounds(p.Image)
 
-	for y := minY; y < maxY; y += 2 {
-		for x := minX; x < maxX; x++ {
-			r, g, b, _ := p.Image.At(x, y).RGBA()
-			r2, g2, b2, _ := p.Image.At(x, y+1).RGBA()
+	for y := minY; y <= maxY; y += 2 {
+		for x := minX; x <= maxX; x++ {
+			r, g, b, a := p.Image.At(x, y).RGBA()
+			r2, g2, b2, a2 := p.Image.At(x, y+1).RGBA()
+
+			_, _ = a, a2
+
+			if a == 0 && a2 == 0 {
+				sb.WriteRune(emptyBlock)
+				continue
+			}
+
+			if a == 0 {
+				sb.WriteString(fmt.Sprintf("%s%c", fmt.Sprintf("\033[38;2;%d;%d;%dm", r, g, b), lowerBlock))
+				continue
+			}
+
+			if a2 == 0 {
+				sb.WriteString(fmt.Sprintf("%s%c", fmt.Sprintf("\033[38;2;%d;%d;%dm", r, g, b), upperBlock))
+				continue
+			}
 
 			foreground := fmt.Sprintf("\033[38;2;%d;%d;%dm", r, g, b)
 			background := fmt.Sprintf("\033[48;2;%d;%d;%dm", r2, g2, b2)
-			sb.WriteString(fmt.Sprintf("%s%s%c\033[0m", foreground, background, blockRune))
+			sb.WriteString(fmt.Sprintf("%s%s%c\033[0m", foreground, background, upperBlock))
 		}
 
 		sb.WriteString("\n")
@@ -48,8 +69,10 @@ func findVisibleBounds(m image.Image) (minX, minY, maxX, maxY int) {
 
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			_, _, _, a := m.At(x, y).RGBA()
-			if a > 0 {
+			r, g, b, a := m.At(x, y).RGBA()
+
+			// Include non-transparent and black pixels in bounds
+			if a > 0 || (r == 0 && g == 0 && b == 0 && a == 65535) {
 				if x < minX {
 					minX = x
 				}
