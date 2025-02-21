c21a09d7ce176fd00e8cd310ddc448e2684a31d9
Phuc Tran <glup3.tran@gmail.com>
2025-02-21 10:42:39 +0100

    init cobra cli

---
+++ README.md
@@ -0,0 +1,16 @@
+# Smeargle
+
+CLI tool to print Pokémon images in the terminal
+
+## Roadmap
+
+- [ ] CLI
+- [x] convert image to unicode
+- [ ] print command
+- [ ] list command
+- [ ] random command
+- [ ] interactive mode (using bubbletea)
+- [ ] shiny flag
+- [ ] name flag
+- [ ] form flag
+- [ ] man page
--- main.go
+++ main.go
@@ -1,45 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"image"
-	_ "image/png"
-	"log/slog"
-	"os"
-
-	"github.com/glup3/smeargle/pokemon"
-)
+import "github.com/glup3/smeargle/cmd"
 
 func main() {
-	name := "rayquaza"
-
-	p1 := loadPokemon(name, false)
-	p2 := loadPokemon(name, true)
-
-	fmt.Println(p1.String())
-	fmt.Println(p2.String())
-}
-
-func loadPokemon(name string, shiny bool) pokemon.Pokemon {
-	var folder string
-	if shiny {
-		folder = "shiny"
-	} else {
-		folder = "regular"
-	}
-
-	reader, err := os.Open(fmt.Sprintf("images/%s/%s.png", folder, name))
-	if err != nil {
-		slog.Error("unable to open image", slog.Any("error", err))
-		os.Exit(1)
-	}
-	defer reader.Close()
-
-	m, _, err := image.Decode(reader)
-	if err != nil {
-		slog.Error("decoding image failed", slog.Any("error", err))
-		os.Exit(1)
-	}
-
-	return pokemon.NewPokemon("pikachu", m)
+	cmd.Execute()
 }
